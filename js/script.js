3f18124c9874af048ddbb1ca864221b8787013e7
zangiabadi <parisazangiabadi0@gmail.com>
2021-03-16 15:14:49 +0330

    add new login

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// $(document).ready(function() {
+
+//     $(".user").focus
+//     $('.user').css('Color: ', 'red');
+
+//     // $("input").blur(function () {
+//     // $(this.parentNode).css('background-image', 'url(images/curvedinput.gif)');
+//     // });
+
+// });
+$("#userName-id").focusin(function() {
+    $(".user").css('bacjground-color', 'red');
+});
+
+// $("div").focusout(function() {
+//     $(this).attr('class', 'focusedout');
+// });
